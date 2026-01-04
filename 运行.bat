9512d17b1a3cbb821313e7bcee09b1d567ae8067
phonewin <wenchaoceliang@163.com>
2026-01-04 15:45:45 +0800

    1

---
+++ 运行.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "D:\mywork\git-repository\github\Open-AutoGLM\venv\Scripts"
+call activate.bat
+cd /d "D:\mywork\git-repository\github\Open-AutoGLM"
+python main.py --base-url https://open.bigmodel.cn/api/paas/v4 --model "autoglm-phone" --apikey "a0d47dab1e169b5cb72a96eb008b4954.YPYACe6Czy7onBME" "依次点击当前页面列表后然后返回，当点完页面中的书本后，需要向上滑动，直到操作完所有书本"
+pause
+
