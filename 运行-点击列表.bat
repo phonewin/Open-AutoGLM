0a2fa54d41939cafce27ac20b89345368db678cd
phonewin <wenchaoceliang@163.com>
2026-01-29 10:27:47 +0800

    1

---
+++ 运行-点击列表.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "D:\mywork\git-repository\github\Open-AutoGLM\venv\Scripts"
+call activate.bat
+cd /d "D:\mywork\git-repository\github\Open-AutoGLM"
+python main.py --base-url https://open.bigmodel.cn/api/paas/v4 --model "autoglm-phone" --apikey "a0d47dab1e169b5cb72a96eb008b4954.YPYACe6Czy7onBME" "依次点击当前页面列表右侧下载的图标，当，如果下面看不见需要向上滑动，直到操作完所有列表"
+pause
+
